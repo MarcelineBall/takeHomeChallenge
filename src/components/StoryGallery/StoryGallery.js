7a618db5115c5803f2d67653c0615969228e0688
Marceline Ball <marchettiball@gmail.com>
2021-07-28 09:57:35 -0600

    Refactor StoryGallery to fix api call display errors

--- src/components/StoryGallery/StoryGallery.js
+++ src/components/StoryGallery/StoryGallery.js
@@ -4,26 +4,34 @@ import mockData from '../../mockData'
 import retrieveTopStories from '../utils/apiCalls'
 
 const StoryGallery = () => {
-  const [topStories, setTopStories] = useState('')
+  const [topStories, setTopStories] = useState([])
   const [error, setError] = useState('')
+  const [storyList, setStoryList] = useState([])
+
   useEffect(() => {
     retrieveTopStories()
-      .then(data => setTopStories(data.results))
-      .catch(error => setError('Sorry, there seems to be an issue retrieving the stories'))
-  }, [])
-  const stories = topStories.map(story => {
-    return (
-      <Card
-      key={Date.now()}
-      section={story.section}
-      title={story.title}
-      abstract={story.abstract} />
-    )
-  })
+    .then(data => setTopStories(data.results))
+    .catch(error => setError('Sorry, there seems to be an issue retrieving the stories'))
+  }, [setTopStories]);
+
+  const allStories = (data) => {
+    const stories = data.map(story => {
+      return (
+        <Card
+        key={Date.now()}
+        section={story.section}
+        title={story.title}
+        abstract={story.abstract} />
+      )
+    })
+    return stories
+  }
+
   return(
     <>
-    {stories &&
-      stories}
+      {error &&
+      <h2>{error}</h2>}
+      <section>{allStories(topStories)}</section>
     </>
   )
 }
