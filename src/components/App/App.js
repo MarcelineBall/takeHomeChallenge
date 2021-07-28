9a7cbe7a50e780d632d58f1dfd3476256995eb05
Marceline Ball <marchettiball@gmail.com>
2021-07-28 07:47:50 -0600

    Import Card to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import mockData from '../../mockData'
+import Card from '../Card/Card'
 
 function App() {
   const titles = mockData.map(story => story.title)
+  const stories = mockData.map(story => {
+
+  })
   return (
     <>
     <h1> Popular times </h1>
