2ac4881297670849410d0c295895ad3fb023d232
Marceline Ball <marchettiball@gmail.com>
2021-07-28 07:11:21 -0600

    Add mock data article

---
+++ src/mockData.js
@@ -0,0 +1,83 @@
+{
+  [
+    {
+      section:	"well",
+      subsection:	"live",
+      title:	"A Common Heart Problem That’s Easy to Miss",
+      abstract:	"About three million adults in the U.S. have been diagnosed with A-fib, a heart-rhythm abnormality that’s on the rise. Here’s how to recognize the signs and treat it.",
+      url:	"https://www.nytimes.com/2021/07/26/well/live/afib-warning-signs.html",
+      uri:	"nyt://article/fa619040-9833-5742-af58-2855f87e4b61",
+      byline:	"By Jane E. Brody",
+      item_type:	"Article",
+      updated_date:	"2021-07-26T08:25:38-04:00",
+      created_date:	"2021-07-26T05:00:08-04:00",
+      published_date:	"2021-07-26T05:00:08-04:00",
+      material_type_facet:	"",
+      kicker:	"Personal Health",
+      des_facet:
+        [
+        	"Preventive Medicine",
+        	"Content Type: Service",
+        	"Heart",
+        	"Tests (Medical)"
+        ],
+      org_facet:	[],
+      per_facet:	[],
+      geo_facet:	[],
+      multimedia:
+      [
+        {
+          url:	"https://static01.nyt.com/images/2021/07/27/science/26sci-BRODY-AFIB/26sci-BRODY-AFIB-superJumbo.jpg",,
+          format:	"superJumbo",,
+          height:	1302,,
+          width:	1965,,
+          type:	"image",,
+          subtype:	"photo",,
+          caption:	"",,
+          copyright:	"Gracia Lam",,
+        },
+        {
+        url:	"https://static01.nyt.com/images/2021/07/27/science/26sci-BRODY-AFIB/26sci-BRODY-AFIB-thumbStandard.jpg",
+        format:	"Standard Thumbnail",
+        height:	75,
+        width:	75,
+        type:	"image",
+        subtype:	"photo",
+        caption:	"",
+        copyright:	"Gracia Lam",
+        },
+        {
+        url:	"https://static01.nyt.com/images/2021/07/27/science/26sci-BRODY-AFIB/26sci-BRODY-AFIB-thumbLarge.jpg",
+        format:	"thumbLarge",
+        height:	150,
+        width:	150,
+        type:	"image",
+        subtype:	"photo",
+        caption:	"",
+        copyright:	"Gracia Lam"
+        },
+        {
+        url:	"https://static01.nyt.com/images/2021/07/27/science/26sci-BRODY-AFIB/26sci-BRODY-AFIB-mediumThreeByTwo210.jpg",
+        format:	"mediumThreeByTwo210",
+        height:	140,
+        width:	210,
+        type:	"image",
+        subtype:	"photo",
+        caption:	"",
+        copyright:	"Gracia Lam"
+        },
+        {
+        url:	"https://static01.nyt.com/images/2021/07/27/science/26sci-BRODY-AFIB/26sci-BRODY-AFIB-articleInline.jpg",
+        format:	"Normal",
+        height:	126,
+        width:	190,
+        type:	"image",
+        subtype:	"photo",
+        caption:	"",
+        copyright:	"Gracia Lam",
+        short_url:	"https://nyti.ms/3eY5atD"
+        }
+      ]
+    }
+  ]
+}
